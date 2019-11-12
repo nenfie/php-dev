22d6f0034ece467b04cdf505a058f367cffa393d
nenfie <nenfie@gmail.com>
2019-11-12 17:02:18 +0700

    create folder: php-mvc

---
+++ php-mvc/app/models/User_model.php
@@ -0,0 +1,12 @@
+<?php
+
+class User_model {
+    private $nama = 'Tjoeng Nenfie';
+
+    public function getUser() 
+    {
+        return $this->nama;
+    }
+}
+
+?>
---
+++ php-mvc/app/views/templates/footer.php
@@ -0,0 +1,8 @@
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+<script src="<?= BASEURL; ?>/js/script.js"></script>
+
+
+</body>
+</html>
